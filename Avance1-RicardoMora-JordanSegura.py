97b5d8c82b8fe06c1f177b05079156da5e61c6f1
AbyssalFluff <126825029+AbyssalFluff@users.noreply.github.com>
2024-03-16 20:27:30 -0600

    1

--- Avance1-RicardoMora-JordanSegura.py
+++ Avance1-RicardoMora-JordanSegura.py
@@ -1,17 +1,18 @@
-#Avance 01 de proyecto de Programacion en Python
+#Avance 01 del proyecto de Programación en Python
 #Creado por Ricardo Mora y Jordan Segura
 usuarios=[]
 print("Bienvenido a EcoFriendly!")
 main_while=True
 while main_while:
-    nombre=input("Antes de comenzar...\nEscribe tu nombre: ")
+    nombre=input("\nAntes de comenzar...\nEscribe tu nombre: ")
     usuarios.append(nombre)
+    print("Opciones de Menú:")
     print("""
         0. Salir
         1. Que es la Huella de Carbono
         2. Calcula tu Huella de Carbono
         3. Como mejorar tu Huella de Carbono
-        4.""")
+        4. Cual es el impacto ambiental de la Huella de Carbono""")
     main_opcion=input("A donde quieres ir? ")
     if main_opcion=="0":
         main_while=False
