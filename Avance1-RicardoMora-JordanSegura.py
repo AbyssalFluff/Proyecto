a65eb718d106214642ac9c0ea144377f55d21888
AbyssalFluff <126825029+AbyssalFluff@users.noreply.github.com>
2024-03-16 19:02:53 -0600

    borrado

--- Avance1-RicardoMora-JordanSegura.py
+++ Avance1-RicardoMora-JordanSegura.py
@@ -1,17 +1,2 @@
 #Avance 01 de proyecto de Programacion en Python
 #Creado por Ricardo Mora y Jordan Segura
-print("Bienvenido a ")
-main_while=True
-while main_while:
-    print("""\n0. Exit
-1.
-2.
-3.
-4.""")
-    main_opcion=input("A donde quieres ir? ")
-    if main_opcion==0:
-        main_while=False
-    if main_opcion==1:
-        print("si")
-    else:
-        print("Lo sentimos... Esa opcion no esta disponible.")
