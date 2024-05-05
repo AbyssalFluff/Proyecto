5fa12fceba80b8819474b932a2d095b664cbf39b
AbyssalFluff <126825029+AbyssalFluff@users.noreply.github.com>
2024-05-05 17:45:02 -0600

    1

--- Avance2-RicardoMora-JordanSegura.py
+++ Avance2-RicardoMora-JordanSegura.py
@@ -54,19 +54,19 @@ area_trabajo.create_window(112,200,window=lb_info2)
 
 lb_info3 = tk.Label(pagina_principal, text="La huella de carbono es una medida de la cantidad total de gases de efecto invernadero (GEI)",
                     width=75, height=1, font=("Arial", 12), bg="white")
-area_trabajo.create_window(400,260,window=lb_info3)
+area_trabajo.create_window(350,260,window=lb_info3)
 lb_info4 = tk.Label(pagina_principal, text="emitidos directa o indirectamente por un individuo, organización, evento, producto o servicio",
                     width=75, height=1, font=("Arial", 12), bg="white")
-area_trabajo.create_window(400,270,window=lb_info4)
+area_trabajo.create_window(300,280,window=lb_info4)
 lb_info5 = tk.Label(pagina_principal, text="a lo largo de su ciclo de vida.",
                     width=30, height=1, font=("Arial", 12), bg="white")
-area_trabajo.create_window(400,280,window=lb_info5)
+area_trabajo.create_window(112,300,window=lb_info5)
 lb_info6 = tk.Label(pagina_principal, text="La huella de carbono se expresa generalmente en términos de la cantidad de dióxido de carbono",
                     width=75, height=1, font=("Arial", 12), bg="white")
-area_trabajo.create_window(400,290,window=lb_info6)
-lb_info7 = tk.Label(pagina_principal, text="a lo largo de su ciclo de vida.",
+area_trabajo.create_window(300,320,window=lb_info6)
+lb_info7 = tk.Label(pagina_principal, text="equivalente (CO2e) emitido.",
                     width=30, height=1, font=("Arial", 12), bg="white")
-area_trabajo.create_window(400,280,window=lb_info5)
+area_trabajo.create_window(112,340,window=lb_info7)
 
 #Ejecutamos la pagina principal
 pagina_principal.mainloop()
