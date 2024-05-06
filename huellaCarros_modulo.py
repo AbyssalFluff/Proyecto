c98fb8868ef71fa43e866645a0e424b6a21e3918
Astral Azuma <163621092+JordanSegura26@users.noreply.github.com>
2024-05-05 19:12:06 -0600

    1

--- huellaCarros_modulo.py
+++ huellaCarros_modulo.py
@@ -13,19 +13,19 @@ def huella_carros():
     pagina_huellaCarros.title("Calcula la huella de tu carro")
 
     #Creamos el area de trabajo
-    area_trabajo = tk.Canvas(pagina_huellaCarros, width=540, height=400, bg="White")
+    area_trabajo = tk.Canvas(pagina_huellaCarros, width=640, height=400, bg="White")
     area_trabajo.pack()
 
     #Declaramos donde se abrira la pagina al ejecutarse
-    pagina_huellaCarros.geometry("540x400+1230+100")
+    pagina_huellaCarros.geometry("640x400+1230+100")
 
     #Texto diciendo que hace la pagina
     lb_info1=tk.Label(pagina_huellaCarros, text="Aqui puedes calcular la huella de carbono de tu carro.",
-                  width=40, height=1, font=("Arial", 12), bg="white")
-    area_trabajo.create_window(185,20,window=lb_info1)
+                  width=44, height=1, font=("Arial", 12), bg="white")
+    area_trabajo.create_window(195,20,window=lb_info1)
     lb_info2=tk.Label(pagina_huellaCarros, text="Solo completa la información necesitada.",
-                  width=25, height=1, font=("Arial", 12), bg="white")
-    area_trabajo.create_window(105,40,window=lb_info2)
+                  width=33, height=1, font=("Arial", 12), bg="white")
+    area_trabajo.create_window(150,40,window=lb_info2)
 
     #Labels and Entry for the needed information
     lb_info3=tk.Label(pagina_huellaCarros, text="Nombre:",
@@ -34,10 +34,13 @@ def huella_carros():
     txt_nombre=tk.Entry(pagina_huellaCarros, font=("Arial", 12), width=18)
     area_trabajo.create_window(250,100,window=txt_nombre)
 
-    lb_info4=tk.Label(pagina_huellaCarros, text="Cuanto combustible(en litros) consume tu carro por kilómetro:",
-                  width=13, height=1, font=("Arial", 12), bg="white")
-    area_trabajo.create_window(70,140,window=lb_info4)
+    lb_info4=tk.Label(pagina_huellaCarros, text="Cuanto combustible(en litros) ",
+                  width=23, height=1, font=("Arial", 12), bg="white")
+    area_trabajo.create_window(130,140,window=lb_info4)
+    lb_info5=tk.Label(pagina_huellaCarros, text="consume tu carro por kilómetro:",
+                  width=26, height=1, font=("Arial", 12), bg="white")
+    area_trabajo.create_window(130,160,window=lb_info5)
     txt_combustible=tk.Entry(pagina_huellaCarros, font=("Arial", 12), width=18)
-    area_trabajo.create_window(250,140,window=txt_combustible)
+    area_trabajo.create_window(330,150,window=txt_combustible)
     
     
